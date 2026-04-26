15947970bd82195548e71fc4f2d03b7aee614f6f
aaa21vek-lab <aaa21vek@gmail.com>
2026-04-26 20:44:19 +0200

    Update set-wallpaper.sh

--- scripts/set-wallpaper.sh
+++ scripts/set-wallpaper.sh
@@ -7,7 +7,7 @@ mkdir -p /etc/skel/.config/autostart
 cat <<EOF > /etc/skel/.config/autostart/set-wallpaper.desktop
 [Desktop Entry]
 Type=Application
-Exec=gsettings set org.gnome.desktop.background picture-uri "file:///usr/share/backgrounds/osana/osana_ocean_1.jpg"
+Exec=gsettings set org.gnome.desktop.background picture-uri "file:///usr/share/backgrounds/osana/osana_ocean_10.jpg"
 Hidden=false
 NoDisplay=false
 X-GNOME-Autostart-enabled=true
@@ -15,5 +15,5 @@ Name=Set Ocean Wallpaper
 EOF
 
 # Также устанавливаем для системы в целом (для новых юзеров)
-gsettings set org.gnome.desktop.background picture-uri "file:///usr/share/backgrounds/osana/osana_ocean_1.jpg"
-gsettings set org.gnome.desktop.background picture-uri-dark "file:///usr/share/backgrounds/osana/osana_ocean_1.jpg"
+gsettings set org.gnome.desktop.background picture-uri "file:///usr/share/backgrounds/osana/osana_ocean_10.jpg"
+gsettings set org.gnome.desktop.background picture-uri-dark "file:///usr/share/backgrounds/osana/osana_ocean_10.jpg"

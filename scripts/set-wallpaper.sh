0ef6f2c9de2abb6cd5e3339000b8278554000bfa
aaa21vek-lab <aaa21vek@gmail.com>
2026-04-26 20:38:06 +0200

    Create set-wallpaper.sh

---
+++ scripts/set-wallpaper.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Установка обоев для GNOME
+dbus-launch gsettings set org.gnome.desktop.background picture-uri "file:///usr/share/backgrounds/osana/osana_ocean_1.jpg"
+dbus-launch gsettings set org.gnome.desktop.background picture-uri-dark "file:///usr/share/backgrounds/osana/osana_ocean_1.jpg"

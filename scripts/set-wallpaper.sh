4af38da729fcb9857304a3d0da71fb85c262bf04
aaa21vek-lab <aaa21vek@gmail.com>
2026-04-26 20:42:48 +0200

    Update set-wallpaper.sh

--- scripts/set-wallpaper.sh
+++ scripts/set-wallpaper.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
-# Установка обоев для GNOME
-dbus-launch gsettings set org.gnome.desktop.background picture-uri "file:///usr/share/backgrounds/osana/osana_ocean_1.jpg"
-dbus-launch gsettings set org.gnome.desktop.background picture-uri-dark "file:///usr/share/backgrounds/osana/osana_ocean_1.jpg"
+
+# Создаем скрипт для автозапуска при входе пользователя
+# Это самый надежный способ для Silverblue/GNOME
+mkdir -p /etc/skel/.config/autostart
+
+cat <<EOF > /etc/skel/.config/autostart/set-wallpaper.desktop
+[Desktop Entry]
+Type=Application
+Exec=gsettings set org.gnome.desktop.background picture-uri "file:///usr/share/backgrounds/osana/osana_ocean_1.jpg"
+Hidden=false
+NoDisplay=false
+X-GNOME-Autostart-enabled=true
+Name=Set Ocean Wallpaper
+EOF
+
+# Также устанавливаем для системы в целом (для новых юзеров)
+gsettings set org.gnome.desktop.background picture-uri "file:///usr/share/backgrounds/osana/osana_ocean_1.jpg"
+gsettings set org.gnome.desktop.background picture-uri-dark "file:///usr/share/backgrounds/osana/osana_ocean_1.jpg"
